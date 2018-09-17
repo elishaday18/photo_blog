0e086b524b2fa3750795892829fc4a057c44b347
Elisha Day <elishamday18@gmail.com>
2018-09-16 23:20:53 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls:['app.component.css']
+
+        
+
+})
+export class AppComponent {
+    emoji = ['🎉', '😍', '😜', '👍'];
+    activeEmoji: string;
+    changeEmoji() {
+        this.activeEmoji = this.emoji[Math.floor(Math.random() * this.emoji.length)];
+    }
+    
+}
---
+++ wwwroot/app.cc458d96c5051075ab51.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],[function(t,e,c){"use strict";c(1)},function(t,e){t.exports="// removed by extract-text-webpack-plugin"}]);
+//# sourceMappingURL=app.cc458d96c5051075ab51.js.map
